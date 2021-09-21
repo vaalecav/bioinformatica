e010ab6f73aac83932c687d6e029baa6b9ffccd1
Vale Cavallaro <valecavallaro.m@gmail.com>
2021-09-20 21:29:16 -0300

    Add comments to ej1

--- ej1.pl
+++ ej1.pl
@@ -1,17 +1,23 @@
 use Bio::SeqIO;
 
-my $usage = "Example: perl gbk2fasta.pl in.gbk out.fas\n"; 
+# We run this script as described in the $usage string. 
+my $usage = "Example: perl ej1.pl sequence.gb result_ej1.fas\n"; 
+
+# We retrieve the input and output file from the command of the script execution.
 my $infile = shift or die $usage; 
 my $outfile = shift or die $usage;
 
+# We construct a new Bio::SeqIO object to representate our genbank input file.
 my $seqin = Bio::SeqIO->new( -file   => "$infile",
                              -format => 'Genbank',
                            );
 
+# We construct a new Bio::SeqIO object to representate our fasta output file.
 my $seqout = Bio::SeqIO->new( -file   => ">$outfile",
                               -format => 'Fasta',
                             );
 
+# We generate out fasta file from out genbank file.
 while (my $inseq = $seqin->next_seq) {
    $seqout->write_seq($inseq);
 }
